1c010777b1dc02379967fd210a4c5aca67634005
jpcastro14 <castrovoip@gmail.com>
2025-11-04 23:43:25 -0300

    Feat: Add Fa Icons

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,34 +1,47 @@
 import { useState } from "react";
 import { useAuth } from "../../contexts/authProvider/useAuth";
+import { faAddressCard } from "@fortawesome/free-regular-svg-icons/faAddressCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export function Header() {
   const [open, setOpen] = useState(false);
   const auth = useAuth();
   return (
     <>
-      <div className=" w-full bg-blue-950 justify-center flex h-20 px-5">
+      <div className=" w-full  bg-blue-800 drop-shadow-2xl   justify-center flex h-20 px-5">
         <div className="w-full max-w-7xl flex justify-between items-center text-white ">
-          <h4 className="font-medium text-2xl xl:text-6xl ">SureBroker</h4>
+          <h4 className="font-light text-2xl">SureBroker</h4>
           {auth.email && (
-            <button className="btn btn-warning" onClick={() => setOpen(!open)}>
-              Deslogar
+            <button className="btn btn-circle " onClick={() => setOpen(!open)}>
+              <FontAwesomeIcon icon={faAddressCard} />
             </button>
           )}
         </div>
       </div>
 
-      <dialog id="my_modal_4" className="modal" open={open}>
-        <div className="modal-box w-80 max-w-5xl absolute right-15 top-15 ">
+      <dialog
+        id="my_modal_4"
+        className="modal"
+        open={open}
+        onClose={() => setOpen(!open)}
+      >
+        <div className="modal-box w-80 max-w-5xl">
           <h3 className="font-bold text-lg">Hello!</h3>
           <p className="py-4">Click the button below to close</p>
           <div className="modal-action">
             <form method="dialog">
               {/* if there is a button, it will close the modal */}
-              <button className="btn" onClick={() => setOpen(!open)}>
-                Close
+              <button
+                className="btn btn-error mx-4 "
+                onClick={() => auth.logout()}
+              >
+                Sair
               </button>
-              <button className="btn" onClick={() => setOpen(!open)}>
-                Close
+              <button
+                className="btn btn-success"
+                onClick={() => setOpen(!open)}
+              >
+                Cancelar
               </button>
             </form>
           </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,69 @@
 export function Home() {
   return (
-    <div className="w-full  max-w-xl justify-self-center mt-10">
-      <div className="card card-side bg-base-300 shadow-sm">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-            alt="Movie"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
+    <>
+      <h2 className="text-black text-xl mt-10 pl-4 mx-4">Fila de corretores</h2>
+      <div
+        id="CardWrapper"
+        className=" w-full flex flex-col items-center md:grid grid-cols-3"
+      >
+        <div className=" min-w-l max-w-xl justify-self-center mt-10 mx-2">
+          <div className="w-full max-w-94  card card-side bg-base-300 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                alt="Movie"
+              />
+            </figure>
+            <div
+              id="CardInfo"
+              className=" min-w-44 min-h-72 max-w-44 card-body pl-2 bg-white text-black border rounded  "
+            >
+              <span className="absolute rounded-full bg-warning p-1 -top-3 right-2">
+                <p className="text-black">Corretor da vez</p>
+              </span>
+              <h2 className="card-title">Silvio Rodrigues</h2>
+              <span className="border border-success rounded-full pl-1 flex justify-center ">
+                Equipe Breno
+              </span>
+              <div className="card-actions justify-start">
+                <span className=" rounded p-1 flex flex-col justify-center ">
+                  <p className="text-xl">Vendas: 4 </p>
+                  <p className="text-slate-400 border-b">Le Parc </p>
+                  <p className="text-slate-400 border-b">Bothanic </p>
+                  <p className="text-slate-400 border-b">Urban Life</p>
+                  <p className="text-slate-400 border-b">Urban Life</p>
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" min-w-l max-w-xl justify-self-center mt-10 mx-2">
+          <div className="card w-full max-w-94 card-side bg-base-300 shadow-sm">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+                alt="Movie"
+              />
+            </figure>
+            <div
+              id="CardInfo"
+              className="  min-w-44 min-h-72 max-w-44 card-body pl-2 bg-white text-black border rounded  "
+            >
+              <h2 className="card-title">Bethania lalalala</h2>
+              <span className="border border-error rounded-full pl-1 flex justify-center ">
+                Equipe Mara
+              </span>
+              <div className="card-actions justify-start">
+                <span className=" rounded p-1 flex flex-col justify-center ">
+                  <p className="text-xl">Vendas: 2 </p>
+                  <p className="text-slate-400 border-b">Le Parc - 12/04 </p>
+                  <p className="text-slate-400 border-b">Bothanic </p>
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
